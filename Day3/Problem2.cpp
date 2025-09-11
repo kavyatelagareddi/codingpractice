4c5d5bd45e393f8df239744b4886ccdfca5411be
Kavya Telagareddi <kavyatelagareddi@gmail.com>
2025-09-11 21:38:33 +0530

    Day3: selection sort

---
+++ Day3/Problem2.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    void insertionSort(vector<int>& arr) {
+        // code here
+        int n=arr.size();
+        for(int i=0;i<=n-1;i++){
+           int j=i;
+           while(j>0 && arr[j-1]>arr[j]){
+               swap(arr[j-1],arr[j]);
+               j--;
+           }
+        }
+    }
+};
