df89e193e754fa9fe759cedd51df07f8b7f0bab4
Kavya Telagareddi <kavyatelagareddi@gmail.com>
2025-09-14 17:32:57 +0530

    Day5:quicksort

---
+++ Day5/Problem2.cpp
@@ -0,0 +1,29 @@
+class Solution {
+  public:
+    void quickSort(vector<int>& arr, int low, int high) {
+        // code here
+        if(low<high){
+            int q=partition(arr,low,high);
+            quickSort(arr,low,q-1);
+            quickSort(arr,q+1,high);
+        }
+       
+    }
+
+  public:
+    int partition(vector<int>& arr, int low, int high) {
+        int pivot=arr[high];
+         int i=low-1;
+        for(int j=low;j<high;j++){
+            
+            if(arr[j]<pivot){
+                i++;
+                swap(arr[i],arr[j]);
+                
+            }
+            
+        }
+        swap(arr[i+1],arr[high]);
+        return(i+1);
+    }
+};
